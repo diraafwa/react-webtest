c1088c7e76055d0572075e826965e344a889c81d
diraafwa <47936646+diraafwa@users.noreply.github.com>
2021-12-04 06:35:59 +0700

    Add files via upload

---
+++ src/pages/Product.js
@@ -0,0 +1,165 @@
+const product_card =  [
+     {
+         id: 1,
+         product_name: "Tempat Sikat",
+         price:10000,
+         currency:"IDR",
+         img:"https://assets.tokodistributor.com/product/f3eb4f9535a53a1cc4a303cfa4c84a2a.jpeg"
+
+     },
+     {
+        id: 2,
+        product_name: "Rak Kamar Mandi",
+        price:30000,
+        currency:"IDR",
+        img:"https://assets.tokodistributor.com/product/050930a877aa0617299939258ccc75dd.jpeg",
+
+    },
+    {
+        id: 3,
+        product_name: "Kaos Oversize Tedy",
+        price:70000,
+        currency:"IDR",
+        img:"https://assets.tokodistributor.com/product/598740886df0ff3b8301fa40d172e06a.jpeg",
+
+    },
+    {
+        id: 4,
+        product_name: "Cream dan Serum",
+        price:70000,
+        currency:"IDR",
+        img:"https://assets.tokodistributor.com/product/ff4144e10623504c983634fcd8a2abd7.jpeg",
+
+    },
+    {
+        id: 5,
+        product_name: "MS Glow Day Cream dan Night Cream",
+        price:100000,
+        currency:"IDR",
+        img:"https://assets.tokodistributor.com/product/c67124dcf5da863d57e5738778826058.jpeg",
+
+    },
+    {
+        id: 6,
+        product_name: "Hoodie White Bear Bahan Fleece",
+        price:133000,
+        currency:"IDR",
+        img:"https://assets.tokodistributor.com/product/a125c3133161baf0beeb8474b713d303.jpeg",
+
+    },
+    {
+        id: 7,
+        product_name: "Meja Serbaguna Portable",
+        price:70000,
+        currency:"IDR",
+        img:"https://assets.tokodistributor.com/product/6595fdc4a76cb0e7e5deaba995365ab7.jpeg",
+
+    },
+    {
+        id: 8,
+        product_name: "Test Images",
+        price:140000,
+        currency:"IDR",
+        img:"https://assets.tokodistributor.com/product/938d45f1fa62687622c50b5004e6f8dd.jpg",
+
+    },
+    {
+        id: 9,
+        product_name: "Jijahe Clothes",
+        price:525000,
+        currency:"IDR",
+        img:"https://assets.tokodistributor.com/product/0304b516ab88d74d795111c92bf28352.jpg",
+
+    },
+    {
+        id: 10,
+        product_name: "Serimbit",
+        price:195000,
+        currency:"IDR",
+        img:"https://assets.tokodistributor.com/product/ddddd3efd48d46c94d4413c6659caa4d.jpeg",
+
+    },
+    {
+        id: 11,
+        product_name: "Sepatu Anak Muda",
+        price:9000,
+        currency:"IDR",
+        img:"https://assets.tokodistributor.com/product/af27bf3f72a2b3f02e741d0c2fd4ff76.jpg",
+
+    },
+    {
+        id: 12,
+        product_name: "Sepatu 1",
+        price:2000,
+        currency:"IDR",
+        img:"https://assets.tokodistributor.com/product/d660954be58168f0eaa46438519aa857.jpg",
+
+    },
+    {
+        id: 13,
+        product_name: "Sepatu 2",
+        price:3000,
+        currency:"IDR",
+        img:"https://assets.tokodistributor.com/product/f44df58649de98264e31b267668052b5.jpg",
+
+    },
+    {
+        id: 14,
+        product_name: "Sepatu 3",
+        price:11000,
+        currency:"IDR",
+        img:"https://assets.tokodistributor.com/product/111f11d1b37838038b1210acb62cd3e0.jpg",
+
+    },
+    {
+        id: 15,
+        product_name: "Sepatu 4",
+        price:30000,
+        currency:"IDR",
+        img:"https://assets.tokodistributor.com/product/826a602c85430a75943c88555a210660.jpg",
+
+    },
+    {
+        id: 16,
+        product_name: "Sepatu 5",
+        price:40000,
+        currency:"IDR",
+        img:"https://assets.tokodistributor.com/product/685ddd53e0c7b494bc48cf0e7eb7d0df.jpg",
+
+    },
+    {
+        id: 17,
+        product_name: "Sepatu 7",
+        price:100000,
+        currency:"IDR",
+        img:"https://assets.tokodistributor.com/product/d1a2af7a32b4154dc0dc23eafdc715d1.jpg",
+
+    },
+    {
+        id: 18,
+        product_name: "Coba Pakai",
+        price:120000,
+        currency:"IDR",
+        img:"https://assets.tokodistributor.com/product/4939ce370e58fc41185d4163e9906f74.jpg",
+
+    },
+    {
+        id: 19,
+        product_name: "Update Gamis",
+        price:200000,
+        currency:"IDR",
+        img:"https://assets.tokodistributor.com/product/cbd6d311d13ae7b9b714861dcc75286c.jpg",
+
+    },
+    {
+        id: 20,
+        product_name: "Produk 1",
+        price:5000,
+        currency:"IDR",
+        img:"https://assets.tokodistributor.com/product/347bb6bdf2137a922c76fb1e7fd1eb2d.jpg",
+
+    },
+]
+
+export default product_card;
+ 
